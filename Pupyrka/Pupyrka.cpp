764dbf655831ea8c2791a0d537416ab452a3ec50
andreysusakov <andrey.susakov@yandex.ru>
2022-02-24 16:10:59 +0300

    Cernovik

--- Pupyrka/Pupyrka.cpp
+++ Pupyrka/Pupyrka.cpp
@@ -4,38 +4,30 @@
 using namespace std;
 
 bool filling (){
+    int s = 1;
     bool a [12][12];
-    for (int i =0; i < 12; i++){
-        for (int j = 0; j < 12; j++){
-            a [i][j] = true;
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < 12; i++){
+        for (j = 0; j < 12; j++){
+            a [i][j] = s;
         }
     }
     return a;
 }
 
 
-void condition (bool s = true){
-    bool a [12][12];
+void condition (bool a [][12]){
+//    bool a [12][12];
     for (int i = 0; i < 12; i++){
         for (int j = 0; j < 12; j++){
-            a [i][j] = s ;
-        }
-    }
-
-    for (int i = 0; i < 12; i++){
-        for (int j = 0; j < 12; j++){
+//            a [i][j] = s;
             cout << a [i][j];
         }
         cout << "\n";
     }
 }
 
-//bool a [12][12];
-//for (int i = ; i < 12; i++){
-//    for (int j = 0; j < 12; j++){
-//        a [i][j] = s ;
-//    }
-//}
 
 
 
@@ -46,28 +38,36 @@ int main()
     int x3 = 0;
     int x4 = 0;
     bool ok = true;
+    bool a [12][12] = filling();
 
-    bool pup = filling ();
-    condition (pup);
+    condition (a);
 
-    bool a [12][12];
+
 
     cout << "Enter the coordinates of the beginning and end of the region\n";
     cin >> x1 >> x2 >> x3 >> x4;
 
     for (int i = x1; i != x3 ; i++){
         for (int j = x2; j != x4; j++){
-            if ((i < 0 && j < 0) || (i >= 12 && j >= 12)){
-                ok = false;
-            }else if (a [i][j] == true) {
-                cout << "Pop!";
+            a [i][j] = filling(ok);
+            if (a [i][j] == true){
                 a [i][j] = false;
+                cout << "Pop!";
+
             }
+//            else if ((i < 0 || j < 0 || i >= 12 || j >= 12)) {
+//                ok = false;
+//                break;
+//            }
         }
+//        if (!ok) {
+//            cout << "Values out of range!";
+//        }
+        cout << endl;
     }
-    if (!ok) {
-        cout << "Values out of range!";
-    }
+    cout << ok << endl;
+    condition(a);
+
 
 
     return 0;
