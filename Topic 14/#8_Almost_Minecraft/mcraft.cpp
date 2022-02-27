d389124d269140531441e74b7896b6a2214d1a8a
andreysusakov <95646945+andreysusakov@users.noreply.github.com>
2022-02-27 13:15:48 +0300

    Mcraft_final

--- Topic 14/#8_Almost_Minecraft/mcraft.cpp
+++ Topic 14/#8_Almost_Minecraft/mcraft.cpp
@@ -3,29 +3,59 @@
 
 using namespace std;
 
-int main()
-{
+void filling (bool a [][5][10]){
+    bool s = false;
+    int i = 0;
+    int j = 0;
     int k = 0;
-    bool l = false;
-    bool mcraft [5][5][10];
+    for (i = 0; i < 5; i++){
+        for (j = 0; j < 5; j++){
+            for (k = 0; k < 10; k++){
+                a [i][j][k] = s;
+            }
+        }
+    }
+}
 
+
+void condition (bool a [][5][10]){
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
             for (int k = 0; k < 10; k++) {
-                cout << "Set block level " << k << " in sector " << i << ";" << j << ", enter 0 or 10" << endl;
-                cin >> l;
-                mcraft [i][j][k] = l;
+            cout << a [i][j][k];
+            }
+        }
+        cout << "\n";
+    }
+}
 
+int main()
+{
+    int cut = 0;
+    int l = 0;
+    int ok = true;
+    bool mcraft [5][5][10];
+
+    filling(mcraft);
+    condition(mcraft);
+
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cout << "Set block level " << " in sector " << i << ";" << j << ", enter 0 or 9" << endl;
+            cin >> l;
+            for (int k = 0; k < l; k++) {
+            mcraft [i][j][k] = ok;
             }
         }
     }
 
     cout << "Enter cut level: \n";
-    cin >> k;
+    cin >> cut;
 
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
-            cout << mcraft [i][j][k];
+            cout << mcraft [i][j][cut];
         }
         cout << "\n";
     }
