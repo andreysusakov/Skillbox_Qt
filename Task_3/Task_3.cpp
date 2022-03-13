b912a62be0ca15fd37a4d44e15ee6886fb18792e
andreysusakov <95646945+andreysusakov@users.noreply.github.com>
2022-03-13 16:02:12 +0300

    Task_3

--- Task_3/Task_3.cpp
+++ Task_3/Task_3.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int n = 0;
+    vector <int> a;
+
+    cout << "Enter vector element: \n";
+    cin >> n;
+    while(n != -2){
+        if(n == -1 & a.size() < 5){
+            cout << "Mistake! Vector size less than 5!";
+        }else if(n == -1){
+            cout << a[4] << endl;
+        }else{
+            a.push_back(n);
+            for(int i = 0; n < a.size(); i++){
+                if(n < a[i]){
+                    swap(a[i], n);
+                }
+            }
+
+        }
+        cout << "Enter vector element: \n";
+        cin >> n;
+
+    }
+
     return 0;
 }
