5463db0b0e5e440696c1e5bba4f7faf22efb3c16
andreysusakov <andrey.susakov@yandex.ru>
2022-03-16 17:20:00 +0300

    Task_3_1.1

---
+++ Task_4/Task_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    vector <int> vec = {-100,-50, -5, 1, 10, 15};
+
+    cout << "Hello World!" << endl;
+    return 0;
+}
--- Topic 15/Task_3/Task_3.cpp
+++ Topic 15/Task_3/Task_3.cpp
@@ -9,30 +9,37 @@ int main()
     int n = 0;
     vector <int> a;
 
-    cout << "Enter vector element: \n";
-    cin >> n;
-    while(n != -2){
+
+    do{
+        cout << "Enter vector element: \n";
+        cin >> n;
         if(n == -1 & a.size() < 5){
             cout << "Mistake! Vector size less than 5!";
         }else if(n == -1){
-            cout << a[4] << endl;
+            cout << a[4] << endl;        
         }else{
             a.push_back(n);
-            for(int i = a.size() - 1; i > 0; i--){
-                if(a[i] < a[i - 1]){
-                    swap(a[i], a[i - 1]);
+            if(a.size() >= 5 & a[4] < n){
+                for(int i = 3; i > 0; i--){
+                    if(a[i] < a[i - 1]){
+                        swap(a[i], a[i - 1]);
+                    }
+                }
+            }else{
+                for(int i = a.size() - 1; i > 0; i--){
+                    if(a[i] < a[i - 1]){
+                        swap(a[i], a[i - 1]);
+                    }
                 }
             }
 
         }
+
         for(int i = 0; i < a.size(); i++){
             cout << a[i] << " ";
         }
 
-        cout << "Enter vector element: \n";
-        cin >> n;
-
-    }
+    }while(n != -2);
 
     return 0;
 }
