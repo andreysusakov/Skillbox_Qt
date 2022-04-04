1e334e78356645a948e1ceba9e1a890a14dc5670
andreysusakov <andrey.susakov@yandex.ru>
2022-04-04 15:00:56 +0300

    Task 2. Stapler

--- Topic_16/Task 2. Stapler/Task 2. Stapler.cpp
+++ Topic_16/Task 2. Stapler/Task 2. Stapler.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <cstdio>
+#include <string>
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    double result;
+    int whole_part = 0;
+    int fractional_part = 0;
+    string z;
+
+    cout << "Enter the integer part of the number: \n";
+    cin >> whole_part;
+    cout << "Enter the fractional part of the number: \n";
+    cin >> fractional_part;
+    string x = to_string(whole_part);
+    string y = to_string(fractional_part);
+    z = x + '.' +y;
+    cout << "Text: " << z << endl;
+
+    result = stod(z);
+    cout << "Number: " << result << endl;
+
     return 0;
 }
---
+++ Topic_16/Task 2. Stapler/cmake-build-debug/CMakeFiles/Task_2__Stapler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task_2__Stapler.dir/Task_2._Stapler.cpp.obj"
+  "Task_2__Stapler.exe"
+  "Task_2__Stapler.exe.manifest"
+  "Task_2__Stapler.pdb"
+  "libTask_2__Stapler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Task_2__Stapler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
