ecc0b11a39d9789260a9c562b21732ca6ee8f24c
andreysusakov <andrey.susakov@yandex.ru>
2022-04-04 17:03:01 +0300

    Task 3. Calculator

---
+++ Topic_16/Task 3. Calculator/Task 3. Calculator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main() {
+//    stringstream example;
+    string example;
+    string buffer;
+    double  result;
+    double x;
+    double y;
+    char z;
+    stringstream buffer_stream;
+
+    cout << "Enter an first example: " << endl;
+    cin >> x >> z >> y;
+
+    buffer_stream << x << z << y;
+    cout << buffer_stream.str() << endl;
+    buffer_stream >> buffer >> x >> buffer >> z >> buffer >> y;
+    cout << x << " " << z << " " << y << endl;
+    result = x + z + y;
+    cout << result << endl;
+
+    return 0;
+}
---
+++ Topic_16/Task 3. Calculator/cmake-build-debug/CMakeFiles/Task_3__Calculator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task_3__Calculator.dir/Task_3._Calculator.cpp.obj"
+  "Task_3__Calculator.exe"
+  "Task_3__Calculator.exe.manifest"
+  "Task_3__Calculator.pdb"
+  "libTask_3__Calculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Task_3__Calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
