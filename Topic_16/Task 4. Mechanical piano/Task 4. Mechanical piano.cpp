160d85bb28239b8f84a29dfc261dad50e2d9d5a6
andreysusakov <95646945+andreysusakov@users.noreply.github.com>
2022-04-30 21:35:47 +0300

    piano

--- Topic_16/Task 4. Mechanical piano/Task 4. Mechanical piano.cpp
+++ Topic_16/Task 4. Mechanical piano/Task 4. Mechanical piano.cpp
@@ -1,35 +1,64 @@
 #include <iostream>
-#include <cstdio>
 #include <string>
 #include <sstream>
 using namespace std;
-enum note
+enum notes
 {
-    DO = 1,
-    RE = 2,
-    MI = 4,
-    FA = 8,
-    SOL = 16,
-    LA = 32,
-    SI = 64
+    DO = 1 << 0,
+    RE = 1 << 1,
+    MI = 1 << 2,
+    FA = 1 << 3,
+    SOL = 1 << 4,
+    LA = 1 << 5,
+    SI = 1 << 6
 };
+
+
 int main() {
-    int note = 0;
-    int n = 0;
-    string melody [12];
-    string chord;
+    string text;
+    int melody [12];
+    int chord;
 
 
-
-
-        for (int i = 0; i < 12; i++){
-            cout << "Enter a chord: " << i + 1 << endl;
-            cin >> chord;
-            melody [i] = chord;
+    for (int i = 0; i < 12; i++){
+        cout << "Enter a chord: " << endl;
+        cin >> chord;
+        melody[i] = chord;
+    }
+    for (int chord_number = 0; chord_number < 12; chord_number++){
+        text = to_string(melody[chord_number]);
+        for (int k = 0; k < text.size(); k++){
+            if (!(chord_number & DO) && text[k] == '1'){
+                chord_number |= DO;
+                cout << "DO" << " ";
+            }
+            else if (!(chord_number & RE) && text[k] == '2'){
+                chord_number |= RE;
+                cout << "RE" << " ";
+            }
+            else if (!(chord_number & MI) && text[k] == '3'){
+                chord_number |= MI;
+                cout << "MI" << " ";
+            }
+            else if (!(chord_number & FA) && text[k] == '4'){
+                chord_number |= FA;
+                cout << "FA" << " ";
+            }
+            else if (!(chord_number & SOL) && text[k] == '5'){
+                chord_number |= SOL;
+                cout << "SOL" << " ";
+            }
+            else if (!(chord_number & LA) && text[k] == '6'){
+                chord_number |= LA;
+                cout << "LA" << " ";
+            }
+            else if (!(chord_number & SI) && text[k] == '7'){
+                chord_number |= SI;
+                cout << "SI" << " ";
+            }
         }
+        cout << endl;
+    }
 
-
-
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
