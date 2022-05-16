bea5a472e2b3e4f0474cf4a5b11ef9e2b134f963
andreysusakov <95646945+andreysusakov@users.noreply.github.com>
2022-05-16 18:10:53 +0300

    Topic_17_fin

--- Topic_17/Task_17_3/Task_17_3.cpp
+++ Topic_17/Task_17_3/Task_17_3.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main()
 {
-    char* text = "Hello World!" << endl;
+    char s[] = "paparapapa";
+    char c[] = "rt";
+    int i, j;
+    bool found = false;
+
+    for(i = 0; *(s + i) != '\0'; i++){
+        bool corr = true;
+        for(j = 0; *(c + j) != '\0'; j++){
+            if(*(c + j) != *(s + (i + j))){
+                corr = false;
+                break;
+            }
+        }
+        if(corr){
+           found = true;
+           cout << "Yes\n";
+           break;
+        }
+    }
+
+
     return 0;
 }
